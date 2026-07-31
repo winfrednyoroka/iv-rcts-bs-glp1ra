724f7e4c67e7176f63cc7118ec034b43ec78273a
winfrednyoroka <winfredgatua13@gmail.com>
2026-07-31 17:25:48 +0100

    add a script for visualising studyoutcomes

---
+++ scripts/bs/01_study_outcome_visualisation.R
@@ -0,0 +1,9 @@
+###################################################################################################
+# Script for visualising the outcomes per study duration versus RCT studies ordered by year
+# By looking at the dotplot visualisation, I can see BMI, SBP, DBP, HTN separately or combined
+# Decision making in IV estimates (BMI measured before BP or at same time point(s))
+#
+# Initial visualisation of study-level outcome availability and follow-up 
+# schedules will be conducted prior to IV analyses to assess evidence coverage,
+# identify sparsity across outcomes and time points to inform analysis and reporting strategy.
+###################################################################################################
