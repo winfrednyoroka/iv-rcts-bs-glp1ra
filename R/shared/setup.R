3e314d05fd2b53530666533f035f9095ceb76cab
Winfred_Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2026-07-31 17:02:54 +0100

    track r-setup for librariesand custom functions (#3)

---
+++ R/shared/setup.R
@@ -0,0 +1,17 @@
+#########################################################
+# Script for loading packages and shared custom functions
+#########################################################
+##### Loading packages only----
+library(readxl)
+library(ggplot2)
+library(tidyverse)
+library(ggbreak) # Cite the use of ggbreak
+library(ggpattern)
+library(patchwork)
+library(testthat) # R package for testing my customised functions
+library(stringr)
+library(ggrepel)
+library(forcats)
+library(tibble)
+library(netmeta)
+library(purrr)
