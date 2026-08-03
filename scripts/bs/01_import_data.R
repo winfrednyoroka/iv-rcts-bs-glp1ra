c5990b93487da53e3c4f29586ebeae5e7804ec52
Winfred_Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2026-08-03 14:52:49 +0100

    Update trial arms with missing female count and proportion (#11)
    * rename some studysheet and trialarm columns
    
    * rename studysheet and trialarm columns
    
    * a function for updating female count and proportion per arm
    
    * updates female count and proportion and save rds object for bs and glp1ras

--- scripts/bs/01_import_data.R
+++ scripts/bs/01_import_data.R
@@ -15,15 +15,39 @@ file <- 'data/bs/raw/DataExtractionBSWG.xlsm'
 
 ###########################################
 # Read the sheets----
+# Update the column names where needed
 ###########################################
 study_outcome_map <- read_excel(study_outcome, sheet = 'StudyID_Followupmatrix')
 study_outcome_map
 
 studysheet <- read_excel(file, sheet = 'Studysheet', skip = 1) # skips first row which is a header
-studysheet
+glimpse(studysheet)
+studysheet <- studysheet |> 
+  rename('last_name' = 'First author last name',
+         'publication_year' = 'Publication year',
+         'number_treatmentarms' = 'Number of comparisons',
+         'studysheetNotes' = 'Notes'
+         
+  )
+
+glimpse(studysheet)
 
 trialarms <- read_excel(file, sheet = 'TrialArms', skip = 1) # skips first row which is a header
-trialarms
+glimpse(trialarms)
+trialarms <- trialarms |> 
+  rename('baseline_N_per_arm' = 'SampleSize of treatment/comparator',
+         'age' = 'Age of the participants per trial arm',
+         'female_count' = 'Number of females',
+         'female_pct' = 'Proportion (percent) of females',
+         'treatment_arm_nameinitials' = 'TreatmentArmName',
+         'treatment_arm_fullname'='Notes',
+         'treatment_group' = 'TrialArmType',
+         'dosage' = 'Dosage for GLP1RAs',
+         'dosage_units' = 'Dosage units',
+         
+         
+  )
+glimpse(trialarms)
 
 baseline <- read_excel(file,sheet = 'Baselinesheet', skip = 1) # skips first row which is a header
 baseline
