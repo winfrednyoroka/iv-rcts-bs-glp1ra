0db102bc576c2e10d5a7a55dac156dbe9a42478e
Winfred_Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2026-08-04 13:40:41 +0100

    Results (#17)
    * a function for updating missing results reported as change from baseline
    
    * script for updating results-post and from change from baseline
    
    * add script description, load libraries and custom functions

---
+++ scripts/bs/05_results_updatemissingvalues.R
@@ -0,0 +1,17 @@
+##########################################################################################
+# Script for updating the results. 
+# Firstly the script reads in the results from post (actual means) reported 
+# post-intervention and the results based on change from baseline
+# Secondly uses imputestats function to update missing values in change from baseline
+# Thirdly, join change with baseline, and calculate the actual means
+# Fourthly, update the CIs, SDs and SEs for the results (post- actual reported means)
+# Combine with the baseline to obtain the author name and year of publication
+# Finally combine the updated post with updated change and save as rds object
+#########################################################################################
+#################################################
+# Load libraries and custom functions----
+#################################################
+source('R/shared/setup.R')
+source('R/shared/imputechangestats.R')
+source('R/shared/update_missing_CIs.R')
+source('R/shared/update_missing_SDs_SEs.R')
