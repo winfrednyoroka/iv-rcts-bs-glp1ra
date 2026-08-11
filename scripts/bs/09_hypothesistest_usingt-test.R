3d580ae3ef3d31dccacab6483052a42e0c742cd8
winfrednyoroka <winfredgatua13@gmail.com>
2026-08-11 10:12:23 +0100

    t-test script with header

---
+++ scripts/bs/09_hypothesistest_usingt-test.R
@@ -0,0 +1,4 @@
+######################################################################
+# Script for hypothesis testing using two sample t-test----
+# Did the treatment groups start with the same mean BMI, DBP and SBP?
+#####################################################################
