88ad90ccc990c4c90dab4ab243a81604b493d336
winfrednyoroka <winfredgatua13@gmail.com>
2026-08-20 09:32:28 +0100

    add arm name and sample size to the data

--- R/shared/calc_wr_beta_effects.R
+++ R/shared/calc_wr_beta_effects.R
@@ -12,7 +12,11 @@ calc_wr <- function(data,
            post_time_months == outcome_time)
     ) |> 
     pivot_wider(
-      id_cols = study_id,
+      id_cols = c(study_id,
+                  arm_name_Intervention,
+                  arm_name_Control,
+                  samplesize_Intervention,
+                  samplesize_Control),
       names_from = c(Outcome, post_time_months),
       values_from = c(beta, se)
     )
