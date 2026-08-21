0dae038b8ad9678922c24f3bf0b9a484636c1f86
Winfred_Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2026-08-21 15:00:58 +0100

    Results (#41)
    * calculate and visualise the Euclidean distance
    
    * t-test and write out the results
    
    * use a plot fsta function to clour bubles by Instrument strength
    
    * a function for ploting Fstat colored by IV strength
    
    * a function to calculate Fstat for glp1ra
    
    * calculate and visaulise Fstat for glp1ras
    
    * change the gem_point size
    
    * changed the geom point size
    
    * check Simonson (2019) results updates
    
    * modified the breaks representing longer months
    
    * add arm name and sample size to the data
    
    * exclude sample size from the output
    
    * generate effects with armname and sample size and remove studies using similar RCTs but different year of publication
    
    * method choice added as function parameter to allow either REML or FE
    
    * modify create forest plot function
    
    * plot and save the plots using updated plot forest func
    
    * a funtion for calculating beta effects for glp1ras
    
    * IV estimates for 6, 12, 16, 17 and 24 months

--- scripts/glp1ra/11_instrumentalvariableestimation.R
+++ scripts/glp1ra/11_instrumentalvariableestimation.R
@@ -4,8 +4,9 @@
 ################################################################################
 # Load libraries and functions
 source('R/shared/setup.R')
-source('R/shared/calc_wr_beta_effects.R')
+source('R/glp1ra/calc_wr_effectsformultipletimepoints.R')
 source('R/shared/wald_ratioestimator.R')
+
 
 # Read in the data
 base_post <- readRDS('data/glp1ra/processed/baseline_post_relabelmultiarms.rds')
@@ -15,209 +16,210 @@ glimpse(base_post)
 post <- base_post |> 
   filter(post_time_months != 0) # remove all baseline measures
 glimpse(post)
-
-# Align the data for analysis and visualisation----
-controls <-
-  post |>
-  filter(treatment_group == "Control")
-
-interventions <-
-  post|>
-  filter(treatment_group == "Intervention")
-controls
-interventions
-
-# Left join the two datasets
-fdat <-
-  interventions |>
-  left_join(
-    controls,
-    by = c("comparison_id", "post_time_months",'Outcome'),
-    suffix = c("_int", "_ctrl")
-  )
-glimpse(fdat)
-unique(fdat$comparison_id)
-fdat
-
-# Create treatmnet effects first
-effects <- fdat %>%
-  mutate(
-    effect = mean_int - mean_ctrl,
-    se_effect = sqrt(se_int^2 + se_ctrl^2)
-  )
-
-effects |> 
-  select(comparison_id,
-         arm_name_int,
-         arm_name_ctrl,
-         author_year_int,
-         Outcome,
-         post_time_months,
-         effect,
-         se_effect)
-effects
-
-#####################
-# Wald ratio estimates
-####################
-library(dplyr)
-library(tidyr)
-
-wald_ratio <- function(dat, outcome, months) {
-  
-  outcome_col <- paste0("effect_", outcome)
-  se_col <- paste0("se_effect_", outcome)
-  
-  tmp <- dat %>%
-    filter(
-      Outcome %in% c("BMI", outcome),
-      post_time_months == months
-    ) %>%
-    select(
+head(post, n=20)
+# Drop Wharton (2023)
+post <- post |> 
+  filter( !grepl("Wharton", comparison_id))
+post
+post |>
+  count(
+    comparison_id,
+    Outcome,
+    post_time_months,
+    treatment_group
+  ) |>
+  filter(n > 1)
+# # Create treatment effects first
+effects <- post |>
+  select(
+    comparison_id,
+    Outcome,
+    post_time_months,
+    treatment_group,
+    mean,
+    sd,
+    samplesize
+  ) |>
+  pivot_wider(
+    id_cols = c(
       comparison_id,
-      author_year_int,
       Outcome,
-      effect,
-      se_effect
-    ) %>%
-    pivot_wider(
-      names_from = Outcome,
-      values_from = c(effect, se_effect)
-    ) %>%
-    filter(
-      !is.na(effect_BMI),
-      !is.na(.data[[outcome_col]])
+      post_time_months
+    ),
+    names_from = treatment_group,
+    values_from = c(mean, sd, samplesize)
+  ) |> 
+  mutate(
+    beta = mean_Intervention - mean_Control,
+    
+    se = sqrt(
+      (sd_Intervention^2 / samplesize_Intervention) +
+        (sd_Control^2 / samplesize_Control)
     )
-  
-  beta_x <- tmp$effect_BMI
-  se_x <- tmp$se_effect_BMI
-  
-  beta_y <- tmp[[outcome_col]]
-  se_y <- tmp[[se_col]]
-  
-  tmp %>%
-    mutate(
-      wald_ratio = beta_y / beta_x,
-      se_wald = abs(wald_ratio) *
-        sqrt(
-          (se_y / beta_y)^2 +
-            (se_x / beta_x)^2
-        ),
-      lci = wald_ratio - 1.96 * se_wald,
-      uci = wald_ratio + 1.96 * se_wald
-    )
-}
+  ) |>
+  select(
+    comparison_id,
+    Outcome,
+    post_time_months,
+    beta,
+    se
+  )
+glimpse(effects)
 
-#################################################################
-# IV estimate BMI and BP (SBP,DBP) at 6 months
-################################################################
-# BMI and SBP @6 months----
-wr_bmi_sbp6 <- wald_ratio(dat = effects, outcome='SBP', months = 6)
-wr_bmi_sbp6 <- wr_bmi_sbp6 |>  wald_ratio(
+
+########################################################################
+# IV estimate BMI and BP (SBP,DBP) at 6, 12, 16, 17 and 24 months----
+#######################################################################
+# BMI and SBP @6, 12, 16, 17 and 24 months----
+wr_bmi_dbp <- calc_wr(
+  data = effects,
+  exposure_outcome = "BMI",
+  outcome_outcome = "DBP",
+  times = c(6, 12, 16, 17, 24)
+)
+wr_bmi_dbp
+
+wr_bmi_sbp <- calc_wr(
+  data = effects,
+  exposure_outcome = "BMI",
+  outcome_outcome = "SBP",
+  times = c(6, 12, 16, 17, 24)
+)
+wr_bmi_sbp
+
+# BMI and SBP at 6 months
+wr_bmi6_sbp6 <- wr_bmi_sbp |>  wald_ratio(
   beta_y = "beta_SBP_6",
   se_y   = "se_SBP_6",
   beta_x = "beta_BMI_6",
   se_x   = "se_BMI_6"
 )
-wr_bmi_sbp6
+wr_bmi6_sbp6 
 
 # Drop rows wiht NAs
-wr_bmi_sbp6_clean <- wr_bmi_sbp6 |> 
+wr_bmi6_sbp6_clean <- wr_bmi6_sbp6 |> 
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
-wr_bmi_sbp6_clean
+  ) |> 
+  select(comparison_id, beta_BMI_6, beta_SBP_6, WR, WR_SE,WR_Lower95CI,WR_Upper95CI)
+wr_bmi6_sbp6_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi_sbp6_clean, 'data/glp1ra/processed/wr_bmi_sbp_6m.rds')
-write_csv(wr_bmi_sbp6_clean, 'output/glp1ra/tables/wr_bmi_sbp_6m.csv')
+saveRDS(wr_bmi6_sbp6_clean, 'data/glp1ra/processed/wr_bmi6_sbp6.rds')
+write_csv(wr_bmi6_sbp6_clean, 'output/glp1ra/tables/wr_bmi6_sbp6.csv')
 
-# # BMI and DBP @6 months----
-wr_bmi_dbp6 <- calc_wr(data = effects,
-                          exposure_outcome = 'BMI',
-                          exposure_time = 6,
-                          outcome_outcome = 'DBP',
-                          outcome_time = 6)
-wr_bmi_dbp6 <- wr_bmi_dbp6 |>  wald_ratio(
+# # BMI @6 months and DBP @6 months----
+wr_bmi6_dbp6 <- wr_bmi_dbp |>  wald_ratio(
   beta_y = "beta_DBP_6",
   se_y   = "se_DBP_6",
   beta_x = "beta_BMI_6",
   se_x   = "se_BMI_6"
 )
-wr_bmi_dbp6
+wr_bmi6_dbp6
 
 # Drop rows wiht NAs
-wr_bmi_dbp6_clean <- wr_bmi_dbp6 |> 
+wr_bmi6_dbp6_clean <- wr_bmi6_dbp6 |> 
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
-wr_bmi_dbp6_clean
+  ) |> 
+  select(comparison_id, beta_BMI_6, beta_DBP_6, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
+wr_bmi6_dbp6_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi_dbp6_clean, 'data/bs/processed/wr_bmi_dbp6m.rds')
-write_csv(wr_bmi_dbp6_clean, 'output/bs/tables/wr_bmi_dbp6m.csv')
+saveRDS(wr_bmi6_dbp6_clean, 'data/glp1ra/processed/wr_bmi6_dbp6.rds')
+write_csv(wr_bmi6_dbp6_clean, 'output/glp1ra/tables/wr_bmi6_dbp6.csv')
 
-# BMI and SBP @12, 16, 17 and 24 months----
-wr_bmi_sbp12 <- calc_wr(data = effects,
-                          exposure_outcome = 'BMI',
-                          exposure_time = 12,
-                          outcome_outcome = 'SBP',
-                          outcome_time = 12)
-wr_bmi12_sbp12 <- wr_bmi12_sbp12 |>  wald_ratio(
-  beta_y = "beta_SBP_12",
-  se_y   = "se_SBP_12",
-  beta_x = "beta_BMI_12",
-  se_x   = "se_BMI_12"
+# BMI and SBP @16 months----
+wr_bmi16_sbp16 <- wr_bmi_sbp |>  wald_ratio(
+  beta_y = "beta_SBP_16",
+  se_y   = "se_SBP_16",
+  beta_x = "beta_BMI_16",
+  se_x   = "se_BMI_16"
 )
-wr_bmi12_sbp12
+wr_bmi16_sbp16
 
 # Drop rows wiht NAs
-wr_bmi12_sbp12_clean <- wr_bmi12_sbp12 |> 
+wr_bmi16_sbp16_clean <- wr_bmi16_sbp16 |> 
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
-wr_bmi12_sbp12_clean
+  ) |> 
+  select(comparison_id, beta_BMI_16, beta_SBP_16, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
+wr_bmi16_sbp16_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi12_sbp12_clean, 'data/bs/processed/wr_bmi12_sbp12.rds')
-write_csv(wr_bmi12_sbp12_clean, 'output/bs/tables/wr_bmi12_sbp12.csv')
+saveRDS(wr_bmi16_sbp16_clean, 'data/glp1ra/processed/wr_bmi16_sbp16.rds')
+write_csv(wr_bmi16_sbp16_clean, 'output/glp1ra/tables/wr_bmi16_sbp16.csv')
 
-# BMI and DBP @12 months----
-wr_bmi12_dbp12 <- calc_wr(data = effects,
-                          exposure_outcome = 'BMI',
-                          exposure_time = 12,
-                          outcome_outcome = 'DBP',
-                          outcome_time = 12)
-wr_bmi12_dbp12 <- wr_bmi12_dbp12 |>  wald_ratio(
-  beta_y = "beta_DBP_12",
-  se_y   = "se_DBP_12",
-  beta_x = "beta_BMI_12",
-  se_x   = "se_BMI_12"
+# BMI and DBP @16 months----
+wr_bmi16_dbp16 <- wr_bmi_dbp |>  wald_ratio(
+  beta_y = "beta_DBP_16",
+  se_y   = "se_DBP_16",
+  beta_x = "beta_BMI_16",
+  se_x   = "se_BMI_16"
 )
-wr_bmi12_dbp12
 
 # Drop rows with NAs
-wr_bmi12_dbp12_clean <- wr_bmi12_dbp12 |> 
+wr_bmi16_dbp16_clean <- wr_bmi16_dbp16 |> 
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
-wr_bmi12_dbp12_clean
+  ) |> 
+  select(comparison_id, beta_BMI_16, beta_DBP_16, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
+wr_bmi16_dbp16_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi12_dbp12_clean, 'data/bs/processed/wr_bmi12_dbp12.rds')
-write_csv(wr_bmi12_dbp12_clean, 'output/bs/tables/wr_bmi12_dbp12.csv')
+saveRDS(wr_bmi16_dbp16_clean, 'data/glp1ra/processed/wr_bmi16_dbp16.rds')
+write_csv(wr_bmi16_dbp16_clean, 'output/glp1ra/tables/wr_bmi16_dbp16.csv')
+
+# BMI and SBP @17 months----
+wr_bmi17_sbp17 <- wr_bmi_sbp |>  wald_ratio(
+  beta_y = "beta_SBP_17",
+  se_y   = "se_SBP_17",
+  beta_x = "beta_BMI_17",
+  se_x   = "se_BMI_17"
+)
+wr_bmi17_sbp17
+
+# Drop rows wiht NAs
+wr_bmi17_sbp17_clean <- wr_bmi17_sbp17 |> 
+  filter(
+    !is.na(WR),
+    !is.na(WR_SE)
+  ) |> 
+  select(comparison_id, beta_BMI_17, beta_SBP_17, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
+wr_bmi17_sbp17_clean
+
+# Save rds object and csv----
+saveRDS(wr_bmi17_sbp17_clean, 'data/glp1ra/processed/wr_bmi17_sbp17.rds')
+write_csv(wr_bmi17_sbp17_clean, 'output/glp1ra/tables/wr_bmi17_sbp17.csv')
+
+# BMI and DBP @17 months----
+wr_bmi17_dbp17 <- wr_bmi_dbp |>  wald_ratio(
+  beta_y = "beta_DBP_17",
+  se_y   = "se_DBP_17",
+  beta_x = "beta_BMI_17",
+  se_x   = "se_BMI_17"
+)
+wr_bmi17_dbp17
+
+# Drop rows with NAs
+wr_bmi17_dbp17_clean <- wr_bmi17_dbp17 |> 
+  filter(
+    !is.na(WR),
+    !is.na(WR_SE)
+  ) |> 
+  select(comparison_id, beta_BMI_17, beta_DBP_17, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
+wr_bmi17_dbp17_clean
+
+# Save rds object and csv----
+saveRDS(wr_bmi17_dbp17_clean, 'data/glp1ra/processed/wr_bmi17_dbp17.rds')
+write_csv(wr_bmi17_dbp17_clean, 'output/glp1ra/tables/wr_bmi17_dbp17.csv')
 
 # BMI and SBP @24 months----
-wr_bmi24_sbp24 <- calc_wr(data = effects,
-                          exposure_outcome = 'BMI',
-                          exposure_time = 24,
-                          outcome_outcome = 'SBP',
-                          outcome_time = 24)
-wr_bmi24_sbp24 <- wr_bmi24_sbp24 |>  wald_ratio(
+wr_bmi24_sbp24 <- wr_bmi_sbp |>  wald_ratio(
   beta_y = "beta_SBP_24",
   se_y   = "se_SBP_24",
   beta_x = "beta_BMI_24",
@@ -229,19 +231,15 @@ wr_bmi24_sbp24_clean <- wr_bmi24_sbp24 |>
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
+  ) |> 
+  select(comparison_id, beta_BMI_24, beta_SBP_24, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
 wr_bmi24_sbp24_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi24_sbp24_clean, 'data/bs/processed/wr_bmi24_sbp24.rds')
-write_csv(wr_bmi24_sbp24_clean, 'output/bs/tables/wr_bmi24_sbp24.csv')
+saveRDS(wr_bmi24_sbp24_clean, 'data/glp1ra/processed/wr_bmi24_sbp24.rds')
+write_csv(wr_bmi24_sbp24_clean, 'output/glp1ra/tables/wr_bmi24_sbp24.csv')
 # BMI and DBP @24 months----
-wr_bmi24_dbp24 <- calc_wr(data = effects,
-                          exposure_outcome = 'BMI',
-                          exposure_time = 24,
-                          outcome_outcome = 'DBP',
-                          outcome_time = 24)
-wr_bmi24_dbp24 <- wr_bmi24_dbp24 |>  wald_ratio(
+wr_bmi24_dbp24 <- wr_bmi_dbp |>  wald_ratio(
   beta_y = "beta_DBP_24",
   se_y   = "se_DBP_24",
   beta_x = "beta_BMI_24",
@@ -254,10 +252,11 @@ wr_bmi24_dbp24_clean <- wr_bmi24_dbp24 |>
   filter(
     !is.na(WR),
     !is.na(WR_SE)
-  )
+  ) |> 
+  select(comparison_id, beta_BMI_24, beta_DBP_24, WR, WR_SE, WR_Lower95CI, WR_Upper95CI)
 wr_bmi24_dbp24_clean
 
 # Save rds object and csv----
-saveRDS(wr_bmi24_dbp24_clean, 'data/bs/processed/wr_bmi24_dbp24.rds')
-write_csv(wr_bmi24_dbp24_clean, 'output/bs/tables/wr_bmi24_dbp24.csv')
+saveRDS(wr_bmi24_dbp24_clean, 'data/glp1ra/processed/wr_bmi24_dbp24.rds')
+write_csv(wr_bmi24_dbp24_clean, 'output/glp1ra/tables/wr_bmi24_dbp24.csv')
 
